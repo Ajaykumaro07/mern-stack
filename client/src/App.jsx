5ce96edb9bc0ebcb459535971ede85f77e998c03
Ajaykumaro07 <ajaykumarbca688@gmail.com>
2024-04-07 13:49:36 -0700

    Now Working Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Dashboard from './Pages/Dashboard'
 export default function App() {
+
   return (
-    <center>
-    <h1 > Welcome to MERN</h1>
-    </center>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={ <Home/> } />
+    <Route path="/about" element={ <About/> } />
+    <Route path='/signIn' element= { <SignIn />} />
+    <Route path='/signUp' element= { <SignUp />} />
+    <Route path='/dashboard' element= { <Dashboard />} />
+
+    </Routes>
+    </BrowserRouter>
   )
 }
